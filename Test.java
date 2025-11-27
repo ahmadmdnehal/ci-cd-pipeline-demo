d63ea9ee5a3c1015918609dcfc794d7ecd4b6150
Md Nehal Ahmad <nehal.ahmad@paynways.com>
2025-11-27 14:06:11 +0530

    first commit

---
+++ Test.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+import java.util.zip.DataFormatException;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println(new Date());
+        System.out.println("Welcome to the Java programming language.");
+        System.out.println(new DataFormatException());
+        Date Date = new Date();
+        System.out.println(Date.getTime());
+    }
+}
