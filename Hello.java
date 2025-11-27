a3d52982e2e45561720c91b72501878ffaa1a8c6
Md Nehal Ahmad <nehal.ahmad@paynways.com>
2025-11-27 14:24:56 +0530

    third commit , and new class addition

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+    public static void main(String[] args) {
+        String str = "Cron expression generator by Cronhub. Schedule and monitor jobs without any infra work.";
+
+        for(int i =0; i<10; i++){
+            System.out.print(str+" ");
+        }
+        System.out.println();
+    }
+}
